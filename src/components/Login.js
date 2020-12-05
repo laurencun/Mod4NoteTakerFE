0d9e770b044b5558b51e76934490925c7f01b948
laurencun <lauren.busbee@gmail.com>
2020-12-04 20:15:03 -0600

    route to login from sign-out link

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,12 +6,16 @@ export default class Login extends Component {
         const loginStyle = {padding:200, align: 'center'}
         const formStyle = {padding:10}
 
+        const login = () => {
+            this.props.history.push('/')
+        }
+
         return (
             <div style={loginStyle}>
                 <h2>Welcome</h2>
                 <form>
                     <input style={formStyle} name='username' type='text' placeholder="Enter Username"/>
-                    <button style={formStyle}>Login</button>
+                    <button onClick={login} style={formStyle}>Login</button>
                 </form>
             </div>
         )

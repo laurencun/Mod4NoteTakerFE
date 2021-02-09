2befb7328f03737d43efbf217620861dc8a7e300
laurencun <lauren.busbee@gmail.com>
2021-02-09 16:43:26 -0600

    reverted changes in apiRoot.js

--- src/apiRoot.js
+++ src/apiRoot.js
@@ -1,4 +1,4 @@
-const DEV_URL = 'http://localhost:3000/';
+const DEV_ROOT = 'http://localhost:3000/';
 const PROD_URL = 'https://ntkr-be.herokuapp.com/';
 const API_ROOT = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
 

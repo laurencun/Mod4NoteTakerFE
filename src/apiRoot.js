4e4df3e67d57c6daa77ce1e807871815e5ed7c09
laurencun <lauren.busbee@gmail.com>
2021-03-08 11:41:17 -0600

    trouble-shoot api_root

--- src/apiRoot.js
+++ src/apiRoot.js
@@ -1,5 +1,5 @@
-const DEV_URL = 'http://localhost:3001';
+// const DEV_URL = 'http://localhost:3001';
 const PROD_URL = 'https://ntkr-be.herokuapp.com';
-const API_ROOT = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
+const API_ROOT = PROD_URL;
 
 export default API_ROOT

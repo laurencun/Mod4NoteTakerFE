baa58ba349f6e0b380a0ba0ea7515d26bbf2db90
laurencun <lauren.busbee@gmail.com>
2021-03-08 12:27:10 -0600

    added .then to new user on click

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,5 +1,4 @@
 import API_ROOT from '../apiRoot.js'
-import login_success from './auth.js'
 const USER_URL = `${API_ROOT}/users`
 const NEW_USER = 'NEW_USER'
 
@@ -12,24 +11,8 @@ export const createUser = (user) => dispatch =>  {
         body: JSON.stringify(user)
     })
     .then(res => res.json())
-    .then(user => dispatch({
+    .then(user =>   dispatch({
         type: NEW_USER,
         user
-    })
-    )
-    .then(fetch(`${API_ROOT}/auth`, reqObj)
-    .then(resp => resp.json())
-    .then(data => {
-      if (data.error) {
-        this.setState({
-          error: data.error
-        })
-      } 
-      else{
-        login_success(data)
-        localStorage.setItem('my_app_token', data.token)
-      }
-      this.props.history.push('/')
-    })
-    )
+    }))
 }
